17b069aa6852af1fac3ea47f8c8695213577012a
befr <frederic.beaupere@erne.net>
2022-07-20 14:00:40 +0200

    clean up

--- blenderbim/tools/disable_blenderbim_addon.py
+++ blenderbim/tools/disable_blenderbim_addon.py
@@ -1,8 +1,6 @@
-from pathlib import Path
 import bpy
 
 
-# bpy.ops.preferences.addon_install(filepath='/home/shane/Downloads/testaddon.py')
 bpy.ops.preferences.addon_disable(module='blenderbim')
 bpy.ops.wm.save_userpref()
 
--- blenderbim/tools/enable_blenderbim_addon.py
+++ blenderbim/tools/enable_blenderbim_addon.py
@@ -1,8 +1,6 @@
-from pathlib import Path
 import bpy
 
 
-# bpy.ops.preferences.addon_install(filepath='/home/shane/Downloads/testaddon.py')
 bpy.ops.preferences.addon_enable(module='blenderbim')
 bpy.ops.wm.save_userpref()
 
